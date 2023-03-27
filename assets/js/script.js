789f019ffd4659e05dcca38e8e11fd3fb426919e
dutchmims <jameshealy901@gmail.com>
2023-03-27 21:14:10 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,5 +15,37 @@ function suffleImage(){
     })
 }
 
-function clicking()
+
+function clicking(){
+
+    for(let i =0; i<card.length; i++){
+
+
+        front[i].classList.add('show')
+
+        setInterval(() => {
+            front[i].classList.remove('show')
+        }, 2000);
+
+        card[i].addEventListener('click' ,()=>{
+
+            front[i].classList.add('flip')
+           const filppedCard = document.querySelectorAll('.flip')
+
+            if(filppedCard.length == 2){
+
+                container.style.pointerEvents ='none'
+                
+                setInterval(() => {
+                    
+                    container.style.pointerEvents ='all'
+                }, 1000);
+ 
+                match(filppedCard[0] , filppedCard[1])
+            }
+        })
+    }
+}
+
+
 function match(cardOne , cardTwo)
