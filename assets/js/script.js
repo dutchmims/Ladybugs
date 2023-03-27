6e27c5dce98900b3d68639bc0151a1ae06c0641d
dutchmims <jameshealy901@gmail.com>
2023-03-27 21:06:03 +0000

    Add details to the suffle function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 const card = document.querySelectorAll('.cell')
 
 
-function suffleImage()
+suffleImage()
+clicking()
+function suffleImage(){
+
+
+    card.forEach(c=>{
+
+        const num = [...Array(card.length).keys()]
+        const random = Math.floor(Math.random()*card.length)
+
+        c.style.order = num[random]
+    })
+}
+
 function clicking()
 function match(cardOne , cardTwo)
