e16d7fe6f5be29644fb3ac5dba5f8fdc83ad2242
dutchmims <jameshealy901@gmail.com>
2023-05-16 19:35:04 +0000

    Try to fix Javascript issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,179 @@
-// retrieve card (cell) element
-const card = document.querySelectorAll('.cell');
+// retrieve all Elements with class attr of "card"
+const cards = document.querySelectorAll('.card');
 
-// retrieve front element
-const front = document.querySelectorAll('.front');
-
-// retrieve container element
-const container = document.querySelector('.container');
+// retrieve move counter element
+const moveCounter = document.getElementById("move-counter");
 
 // retrieve score element
-const score = document.querySelector('.score span');
+const scoreEl = document.getElementById("score");
+
+// retrieve best score element
+const bestScoreEl = document.getElementById("best-score");
+
+// manage flip state
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard;
+let secondCard;
+
+// store elapsed time from timer
+let timerInterval;
+// second counter for score calculation
+let secCounter = 0;
+// store number of moves
+let numMoves = 0;
+// count matches to determine when game is finished
+let matchCounter = 0;
+
+// store scores
+let bestScores = [];
+
+
+// calculate scores
+function calcScore() {
+    let score = Math.round((10000 - numMoves) / secCounter);
+    // display current score 
+    scoreEl.innerHTML = score;
+    // store all scores in bestScore array
+    bestScores.push(score);
+
+    getBestScore();
+}
+
+// get and display highest number from bestScores array
+function getBestScore() {
+    // get highest value from bestScores array
+    let maxScore = Math.max(...bestScores);
+
+    bestScoreEl.innerHTML = maxScore;
+}
+
+
+/* MEMORY GAME FUNCTIONALITY
+    source: https://medium.com/free-code-camp/vanilla-javascript-tutorial-build-a-memory-game-in-30-minutes-e542c4447eae
+*/
+
+function flipCard() {
+    // count moves
+    numMoves++;
+    moveCounter.innerHTML = numMoves;
+
+    // prevent more than 2 cards from flipping at the same time
+    if (lockBoard) return;
+    // check if current clicked card is equal to first card
+    if (this === firstCard) return;
+
+    // this refers to respective div.card element
+    this.classList.add('flip'); // append class of "flip" to all items of cards array
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // second click
+        secondCard = this;
+
+        checkForMatch();
+    }
+}
+// find matching cards
+function checkForMatch() {
+    if (firstCard.dataset.image === secondCard.dataset.image) {
+        // cards match
+        matchCounter += 1;
+        disableCards();
+        if (matchCounter === (cards.length / 2)) {
+            stopGame();
+        }
+    } else {
+        // cards don't match
+        unflipCards();
+    }
+}
+
+// prevent matched cards from unflipping
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+//turn unmatching cards back after 1.5s
+function unflipCards() {
+
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);
+}
+
+// reset firstCard and secondCard after each round
+function resetBoard() {
+    hasFlippedCard = false;
+    lockBoard = false;
+    firstCard = null;
+    secondCard = null;
+}
 
 // shuffle cards 
-suffleImage();
-clicking();
-function suffleImage() {
-  card.forEach((c) => {
-    const num = [...Array(card.length).keys()];
-    const random = Math.floor(Math.random() * card.length);
-
-    c.style.order = num[random];
-  });
+function shuffle() {
+    for (let card of cards) {
+        let ramdomPos = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPos;
+    }
 }
 
-function clicking() {
-  for (let i = 0; i < card.length; i++) {
-    front[i].classList.add('show');
+// render entire game
+function startGame() {
+    // shuffle cards
+    shuffle();
 
-    setInterval(() => {
-      front[i].classList.remove('show');
-    }, 2000);
 
-// count moves
-    card[i].addEventListener('click', () => {
-      front[i].classList.add('flip');
-      const filppedCard = document.querySelectorAll('.flip');
+    // render player name
+    if (playerNameInput.value === "") {
+        playerName.innerHTML = "Your";
+    } else {
+        playerName.innerHTML = `${playerNameInput.value}'s`;
+    }
 
-      if (filppedCard.length == 2) {
-        container.style.pointerEvents = 'none';
+    closeModal();
 
-        setInterval(() => {
-          container.style.pointerEvents = 'all';
-        }, 1000);
+    startTimer();
 
-        match(filppedCard[0], filppedCard[1]);
-      }
-    });
-  }
+    // add event listener to each item of cards array
+    for (let card of cards) {
+        card.addEventListener('click', flipCard);
+    }
+    // reset move counter to 0
+    numMoves = 0;
+    moveCounter.innerHTML = numMoves;
 }
 
-// find matching cards
-function match(cardOne, cardTwo) {
-  if (cardOne.dataset.index == cardTwo.dataset.index) {
-    score.innerHTML = parseInt(score.innerHTML) + 1;
+// restart game
 
-    cardOne.classList.remove('flip');
-    cardTwo.classList.remove('flip');
+function restartGame() {
+    // storing bestScores array in local storage
+    localStorage.setItem("bestScores", JSON.stringify(bestScores)); // not working!!!
 
-    cardOne.classList.add('match');
-    cardTwo.classList.add('match');
-  } else {
-    setTimeout(() => {
-      cardOne.classList.remove('flip');
-      cardTwo.classList.remove('flip');
-    }, 1000);
-  }
+    //hide restart button
+    restartBtn.style.display = "none";
+
+    // flip all cards back again
+    cards.forEach(card => card.classList.remove('flip'));
+
+    // set matching cards to 0
+    matchCounter = 0;
+
+    // reset score to 0
+    scoreEl.innerHTML = 0;
+    startGame();
 }
+
+
+// restart game button restarts game without reloading the page
+restartBtn.addEventListener("
